7eee6b5e7fed5ad56065b431c0c9603c41c5eed7
Rob Hill <robert.hill0122@gmail.com>
2014-07-30 09:49:22 -0600

    all stories completed

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,8 +1,10 @@
 class Rectangle
+  @@counter = 0
 
   def initialize(length, width)
     @length = length
     @width = width
+    @@counter += 1
   end
 
   def area
@@ -12,4 +14,17 @@ class Rectangle
   def perimeter
     @length * 2 + @width * 2
   end
+
+  def self.counter
+    @@counter
+  end
+
+  def self.create_multiple(number_to_create, length, width)
+    @creation_counter = 0
+    while @creation_counter < number_to_create
+      Rectangle.new(length, width)
+      @creation_counter += 1
+    end
+  end
 end
+
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -13,4 +13,14 @@ describe Rectangle do
   it "can return the perimeter of the rectangle" do
     expect(rectangle.perimeter).to eq(18)
   end
+
+  it "can count the total number of rectangles created" do
+    expect(Rectangle.counter).to eq(2)
+  end
+
+  it "can create multiple instances of the Rectangle object at once" do
+    Rectangle.create_multiple(3, 2, 3)
+    expect(Rectangle.counter).to eq(5)
+  end
+
 end
